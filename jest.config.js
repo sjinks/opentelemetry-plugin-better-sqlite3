baa38b2335626d2503ebd617e9278feceeaa7bc0
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-10-29 22:08:43 +0200

    Remove deprecated 'packageJson' option

--- jest.config.js
+++ jest.config.js
@@ -12,9 +12,4 @@ module.exports = merge.recursive(ts_preset, {
         '<rootDir>/dist/',
         '<rootDir>/node_modules/',
     ],
-    globals: {
-        'ts-jest': {
-            packageJson: 'package.json',
-        },
-    }
 });
