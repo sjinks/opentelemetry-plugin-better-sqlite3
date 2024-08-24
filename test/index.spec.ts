089037f66f56b9ce0f88170b10388a03a4113e83
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-08-24 12:51:02 +0300

    fix: address deprecations

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -8,7 +8,7 @@ import {
     type ReadableSpan,
     SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
-import { SEMATTRS_DB_NAME, SEMATTRS_DB_STATEMENT, SEMATTRS_DB_SYSTEM } from '@opentelemetry/semantic-conventions';
+import { ATTR_DB_NAMESPACE, ATTR_DB_QUERY_TEXT, ATTR_DB_SYSTEM } from '@opentelemetry/semantic-conventions/incubating';
 import { BetterSqlite3Instrumentation } from '../lib';
 
 // The instrumentation must be loaded before the module it is going to instrument
@@ -27,9 +27,9 @@ function checkSpanAttributes(
 ): void {
     equal(spans.name, name);
     equal(spans.status.code, code);
-    equal(spans.attributes[SEMATTRS_DB_SYSTEM], 'sqlite3');
-    equal(spans.attributes[SEMATTRS_DB_NAME], ':memory:');
-    equal(spans.attributes[SEMATTRS_DB_STATEMENT], stmt);
+    equal(spans.attributes[ATTR_DB_SYSTEM], 'sqlite3');
+    equal(spans.attributes[ATTR_DB_NAMESPACE], ':memory:');
+    equal(spans.attributes[ATTR_DB_QUERY_TEXT], stmt);
     equal(spans.status.message, err?.message);
 }
 
